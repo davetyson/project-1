9db1d246a39ab658acef8676385f0c08c298b14e
Dave Tyson <davetysonbass@gmail.com>
2023-02-21 21:01:29 -0500

    added popup JS to script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 // Pop up JS
-// My attempt at making a timed pop up module with a discount code that pops up using JS
 
+// Define variables
+const popUp = document.querySelector('div');
+const closeButton = document.querySelector('button');
+
+// Set some timers so the pop up doesn't display for 5 seconds and transitions in
+setTimeout(() => { popUp.style.display = 'flex'; }, 5000);
+setTimeout(() => { popUp.style.opacity = '1'; }, 5010);
+
+// when the user clicks the button to close the window, the pop up will transition back to a state of invisibility
+closeButton.addEventListener('click', function(){
+    popUp.style.opacity = '0';
+    setTimeout(() => { popUp.style.display = 'none'; }, 1000);
+})
